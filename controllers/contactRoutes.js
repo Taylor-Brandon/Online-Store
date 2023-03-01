e2db198ca206d24689144509747ca448852d4082
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-02-28 20:34:10 -0500

    Added routes

--- controllers/contactRoutes.js
+++ controllers/contactRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
 
+    res.render('contact');
+});
 
 module.exports = router;
--- controllers/orderRoutes.js
+++ controllers/orderRoutes.js
@@ -1,2 +1,8 @@
 const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+
+    res.render('order');
+});
+
 module.exports = router;
