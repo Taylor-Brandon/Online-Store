085b45380fb2987c5eebd7b5b5fe2b66622cd223
Uchenna Obicheta <uobicheta@hotmail.com>
2023-02-28 19:06:52 -0500

    Upadated handlebars and routes.

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,33 @@ const router = require('express').Router();
 
 // const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const loginRoutes = require('./loginRoutes');
+const alllaptopsRoutes = require('./alllaptopsRoutes');
+const doneRoutes = require('./doneRoutes');
+const macbookRoutes = require('./macbookRoutes');
+const orderRoutes = require('./orderRoutes');
+const productDetailsRoutes = require('./productDetailsRoutes');
+const signupRoutes = require('./signupRoutes');
+const subscriptionRoutes = require('./subscriptionRoutes');
+const ultrabooksRoutes = require('./ultrabooksRoutes');
+const ultraportablesRoutes = require('./ultraportablesRoutes');
+const contactRoutes = require('./contactRoutes');
 
-router.use('/', homeRoutes);
+
+//router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
+
+router.use('/home', homeRoutes);
+router.use('/login', loginRoutes);
+router.use('/alllaptops', alllaptopsRoutes);
+router.use('/done', doneRoutes);
+router.use('/macbooks', macbookRoutes);
+router.use('/order', orderRoutes);
+router.use('/product-details', productDetailsRoutes);
+router.use('/signup', signupRoutes);
+router.use('/subscription', subscriptionRoutes);
+router.use('/ultrabooks', ultrabooksRoutes);
+router.use('/ultrportables', ultraportablesRoutes);
+router.use('/contact', contactRoutes);
 
 module.exports = router;
