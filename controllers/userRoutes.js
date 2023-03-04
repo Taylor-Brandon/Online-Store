637ddf070806a28bdc89f988baed592c9bc2067e
Uchenna Obicheta <uobicheta@hotmail.com>
2023-03-03 22:47:07 -0500

    Updated images for orders and product details page.

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -8,6 +8,7 @@ router.get('/', async (req, res) => {
 res.send("In user routes");
 });
 
+
 router.post('/', async (req, res) => {
    console.log("In userRoutes.js");
    console.log(req.body);
@@ -17,18 +18,25 @@ router.post('/', async (req, res) => {
 
    req.session.save(() => {
      req.session.user_id = customerData.id;
+     req.session.firstname = customerData.firstname;
+     req.session.lastname = customerData.lastname;
      req.session.logged_in = true;
-
-     res.status(200).json(customerData);
    });
+
+    console.log(req.session);
     res.status(200).json(customerData);
+  // res.render('home', {
+  //  logged_in: req.session.logged_in,
+  //  firstname: req.session.firstname,
+  // lastname: req.session.lastname
+  // });
   
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
-    
   }
 });
+
 
 
 module.exports = router;
